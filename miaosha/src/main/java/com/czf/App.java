88145aa7d117b7efd74737134c67ebf3ce58c085
清雪冰橙 <1154238323@qq.com>
2020-06-14 22:53:19 +0800

    增加了部分注释

--- miaosha/src/main/java/com/czf/App.java
+++ miaosha/src/main/java/com/czf/App.java
@@ -34,7 +34,7 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "start.." );
         SpringApplication.run(App.class, args);
     }
 }
